fe566348ee24780d1c6a6b9c724b5440c16c38cb
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2026-02-02 11:59:28 +0700

    feat: add validation constraints for AuthRequest, RegisterRequest, and VerifyOtpRequest DTOs

--- fuji/src/main/java/com/example/fuji/dto/AuthRequest.java
+++ fuji/src/main/java/com/example/fuji/dto/AuthRequest.java
@@ -1,5 +1,7 @@
 package com.example.fuji.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthRequest {
+     @NotBlank(message = "Username không được để trống")
+    @Size(min = 4, max = 20, message = "Username phải từ 4 đến 20 ký tự")
     private String username;
+
+    @NotBlank(message = "Mật khẩu không được để trống")
+    @Size(min = 6, max = 32, message = "Mật khẩu phải từ 6 đến 32 ký tự")
     private String password;
 }
--- fuji/src/main/java/com/example/fuji/dto/RegisterRequest.java
+++ fuji/src/main/java/com/example/fuji/dto/RegisterRequest.java
@@ -1,4 +1,7 @@
 package com.example.fuji.dto;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +10,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+
+    @NotBlank(message = "Username không được để trống")
+    @Size(min = 4, max = 20, message = "Username phải từ 4 đến 20 ký tự")
     private String username;
+
+    @NotBlank(message = "Email không được để trống")
+    @Email(message = "Email không đúng định dạng")
     private String email;
+
+    @NotBlank(message = "Mật khẩu không được để trống")
+    @Size(min = 6, max = 32, message = "Mật khẩu phải từ 6 đến 32 ký tự")
     private String password;
+
+    @NotBlank(message = "Họ tên không được để trống")
+    @Size(min = 2, max = 50, message = "Họ tên phải từ 2 đến 50 ký tự")
     private String fullName;
 
 }
